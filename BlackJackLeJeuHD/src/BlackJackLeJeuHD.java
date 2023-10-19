2db4ef86abab6f831b0476ee8f633fc5f6bc5de8
Lapuissancepremier <148439807+Lapuissancepremier@users.noreply.github.com>
2023-10-19 17:38:23 +0000

    Jeu fini  simple sans menu juste les bouton de base

--- BlackJackLeJeuHD/src/BlackJackLeJeuHD.java
+++ BlackJackLeJeuHD/src/BlackJackLeJeuHD.java
@@ -31,6 +31,15 @@ public class BlackJackLeJeuHD {
         public boolean estunAs(){
             return valeur == "A";
         }
+
+        public String getImageChem(){
+            return "./cartes/" + toString() + ".png";
+        }
+
+
+
+
+
         }
 
     ArrayList<Carte> deck;
@@ -47,31 +56,169 @@ public class BlackJackLeJeuHD {
     int joueurSomme;
     int joueurNbAs;
 
-    //fenêtre
-    int fenLarg = 600;
-    int fenHaut = 800;
+
+
+
+
+
+        public void gamewindow(){
+            
+            //fenêtre
+            JFrame fene = new JFrame("BlackJack Le Jeu HD");
+            int fenlarg = 1800;
+            int fenHaut = 1020;
+            fene.setVisible(true);
+            fene.setSize(fenlarg, fenHaut);
+            fene.setLocationRelativeTo(null);
+            fene.setResizable(false);
+            fene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-    JFrame fene = new JFrame("BlackJack Le Jeu HD");
-    JPanel gamePanel = new JPanel();
+    
+            //bouton
+            JPanel bouttonPanel = new JPanel();
+            JButton hitBoutton = new JButton("Hit");
+            JButton stayboutton = new javax.swing.JButton("Stay");
+            hitBoutton.setFocusable(false);
+            bouttonPanel.add(hitBoutton);
+            stayboutton.setFocusable(false);
+            bouttonPanel.add(stayboutton);
+
+    
+    
+    
+    
+            //gamepanel
+            int largCarte = 210;
+            int hautCarte = 300;
+    
+            JPanel gamPanel = new JPanel(){
+                @Override
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+    
+                    try {
+    
+                        //carte face cachée
+                        Image carteFaceCacheImg = new ImageIcon(getClass().getResource("./cartes/DOS.png")).getImage();
+                        if (!stayboutton.isEnabled()){
+                            carteFaceCacheImg = new ImageIcon(getClass().getResource(hiddenCarte.getImageChem())).getImage();
+                        }
+                        g.drawImage(carteFaceCacheImg, 350, 20, largCarte, hautCarte, null);
+    
+                        //carte du dealer
+                        for (int i = 0; i < dealerhand.size();i++){
+                            Carte carte = dealerhand.get(i);
+                            Image carteImg = new ImageIcon(getClass().getResource(carte.getImageChem())).getImage();
+                            g.drawImage(carteImg, largCarte + 360 + (largCarte + 5)*i, 20, largCarte, hautCarte, null);
+
+                        };
+                        
+                        //carte joueur
+                        for (int i = 0; i < mainjoueur.size();i++){
+                            Carte carte = mainjoueur.get(i);
+                            Image carteImg = new ImageIcon(getClass().getResource(carte.getImageChem())).getImage();
+                            g.drawImage(carteImg, 350 + (largCarte + 5)*i, 625, largCarte, hautCarte, null);
+                        }
+
+                        if (!stayboutton.isEnabled()){
+                            dealersomme = reductionAsDealer();
+                            joueurSomme = reductionAsJoueur();
+                            System.out.println("Stay");
+                            System.out.println(dealersomme);
+                            System.out.println(joueurSomme);
+
+                            String message = "";
+                            if (joueurSomme > 21 ) {
+                                message = " PERDU LOOSER";
+                                g.setColor(Color.RED);
+                            }
+                            else if (dealersomme > 21) {
+                                message = "GAGNE GAGNANT";
+                                g.setColor(Color.ORANGE);
+                            }
+                            else if (joueurSomme == dealersomme){
+                                message = " EGALITE DE LOOSER";
+                                g.setColor(Color.WHITE);
+                            }
+                            else if (joueurSomme > dealersomme){
+                                message = "GAGNE GAGNANT";
+                                g.setColor(Color.ORANGE);
+                            }
+                            else if ( joueurSomme < dealersomme) {
+                                message = "PERDU LOOSER";
+                                g.setColor(Color.RED);
+                            }
+                            
+
+                            g.setFont(new Font("Arial", Font.PLAIN, 50));
+                            g.drawString(message, 500, 500);
+                            
+                            
+
+
+
+                        }
+    
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+    
+            };
+            gamPanel.setBackground(new Color(53, 101,77));
+    
+    
+    
+    
+    
+            //affichage
+            fene.add(gamPanel);
+            fene.add(bouttonPanel, BorderLayout.SOUTH);
+
+            hitBoutton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    Carte carte = deck.remove(deck.size()-1);
+                    joueurSomme += carte.getValue();
+                    joueurNbAs += carte.estunAs()? 1 : 0;
+                    mainjoueur.add(carte);
+                    if (reductionAsJoueur() > 21) {
+                        hitBoutton.setEnabled(false);;
+                    }
+                    gamPanel.repaint();
+            }});
+
+            stayboutton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    hitBoutton.setEnabled(false);
+                    stayboutton.setEnabled(false);
+
+                    while (dealersomme < 17) {
+                        Carte carte = deck.remove(deck.size()-1);
+                        dealersomme += carte.getValue();
+                        dealerNbAs += carte.estunAs()? 1 : 0;
+                        dealerhand.add(carte);
+                    }
+                    gamPanel.repaint();
+                }
+            });
+
+
+
+            gamPanel.repaint();
+        }
+        
+    
+
+    
 
     
 
 
     BlackJackLeJeuHD() {
         startGame();
-
-        //fenêtre
-        fene.setVisible(true);
-        fene.setSize(fenLarg, fenHaut);
-        fene.setLocationRelativeTo(null);
-        fene.setResizable(false);
-        fene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        gamePanel.setLayout(new BorderLayout());
-        gamePanel.setBackground(new Color(53, 101,77));
+        gamewindow();
         
         
-    
     System.out.println("execution...");
     }
 
@@ -157,6 +304,22 @@ public class BlackJackLeJeuHD {
         System.out.println(deck);
     }
 
+    public int reductionAsJoueur(){
+        while (joueurSomme > 21 && joueurNbAs > 0) {
+            joueurSomme -= 10;
+            joueurNbAs -= 1;
+        }
+        return joueurSomme;
+    }
+    public int reductionAsDealer() {
+        while (dealersomme > 21 && dealerNbAs > 0) {
+            dealersomme -= 10;
+            dealerNbAs -= 1;
+        }
+        return dealersomme ;
 
 
-}
+    }
+
+    }
+
